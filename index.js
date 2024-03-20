fd14a532fa0decb0f890aaf76f1ec5420e425682
prince mumo <princeinkunzi@gmail.com>
2024-03-20 16:52:25 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+ const companyNamevalue = 'Scuber'
+
+ function companyName() { 
+    expect(companyName).to.equal('Scuber');
+ }
+
+ let mostProfitableNeighborhood = 'Chelsea'
+ function mostProfitableNeighborhood() {
+      expect(mostProfitableNeighborhood).to.equal('Chelsea');
+    } 
+
+let companyCeo='Susan Smith'
+function companyCeo() {
+          expect(companyCeo).to.equal('Susan Smith');
+        }
+
+
+
+
+
